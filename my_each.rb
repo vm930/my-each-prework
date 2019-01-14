c91466502df9c86b756552f4380afd2575f52454
abstract-animation-9914 <vm930@hotmail.com>
2019-01-14 20:45:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,13 @@
 def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
   i = 0
   while i < collection.length 
   yield(collection[i])
   i = i + 1
-  collection.each { |word| puts "#{word}" }
   end
+  
+  array
+else
+  puts "#{word}"
 end
